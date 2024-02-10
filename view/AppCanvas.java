adeb6e338a799e18fbb017923fa82d6bb2732a66
dhouse2 <dhouse2@uco.edu>
2024-02-10 13:50:41 -0600

    final final commit

--- view/AppCanvas.java
+++ view/AppCanvas.java
@@ -2,7 +2,6 @@ package view;
 
 import javax.swing.JPanel;
 
-
 import controller.App;
 import model.GameState;
 import model.Marking;
@@ -14,19 +13,18 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
-public class AppCanvas extends JPanel{
+public class AppCanvas extends JPanel {
 
-public static final int WIDTH = 400;
-public static final int HEIGHT = 100;
-public AppCanvas() {
-    setPreferredSize(new Dimension(WIDTH, HEIGHT));
-}
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 100;
 
+    public AppCanvas() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
 
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
 
         Graphics2D g2 = (Graphics2D) g;
         g2.setFont(new Font("Comic Sans", Font.BOLD, 16));
@@ -34,24 +32,24 @@ public AppCanvas() {
         TicTacToeGame game = App.game;
         GameState gameState = game.getState();
 
-        switch (gameState){
+        switch (gameState) {
             case INIT:
-            g2.setColor(Color.PINK);
-            g2.drawString("Press New Game to start!", 50, 50);
-            break;
+                g2.setColor(Color.PINK);
+                g2.drawString("Press New Game to start!", 50, 50);
+                break;
             case PLAYING:
-            g2.setColor(Color.red);
-            g2.drawString(game.getTurn() + " Turn!", 50, 50);
-            break;
+                g2.setColor(Color.red);
+                g2.drawString(game.getTurn() + " Turn!", 50, 50);
+                break;
             case OVER:
-            g2.setColor(Color.RED);
-            
-           if(game.getWinner() == Marking.U){
-                g2.drawString("Draw! Game Over", 50, 50);
-            } else{
-                g2.drawString(game.getWinner() + " Wins! Game Over", 50, 50);
-            }
-            break;
+                g2.setColor(Color.RED);
+
+                if (game.getWinner() == Marking.U) {
+                    g2.drawString("Draw! Game Over", 50, 50);
+                } else {
+                    g2.drawString(game.getWinner() + " Wins! Game Over", 50, 50);
+                }
+                break;
         }
     }
 }
